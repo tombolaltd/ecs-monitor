b1f0e4accdd169dd904ce722b1bd4e383456e736
Marc Costello <marc.costello@tombola.com>
2018-02-15 18:42:44 +0000

    Fixing a bug where depending on the agent it may not get the correct instance type for the agent tooltip

--- src/components/agents/agentBreakdown/agent/agent.js
+++ src/components/agents/agentBreakdown/agent/agent.js
@@ -3,6 +3,9 @@ import { nameFromAwsArn } from '../../../../utils/stringFormatting';
 import Tooltip from '../../../tooltip/tooltip';
 import './agent.css';
 
+function filterForInstanceType(attributes) {
+    return attributes.name === "ecs.instance-type";
+}
 
 class Agent extends Component {
     get instanceId() {
@@ -16,30 +19,9 @@ class Agent extends Component {
             html: `#tooltip-${this.instanceId}`
         };
     }
-    
-    constructor(props) {
-        super(props);
-        this.handleMouseOverIcon = this.handleMouseOverIcon.bind(this);
-        this.setEc2DetailState = this.setEc2DetailState.bind(this);
-        
-        this.state = {
-            ec2Details: {}
-        };
-    }
-    
+
     getColour(taskDefinitionArn) {
         return this.props.taskDefinitionColours[taskDefinitionArn];
-    }
-    
-    setEc2DetailState(ec2Details) {
-        this.setState({ ec2Details });
-        return ec2Details;
-    }
-
-    handleMouseOverIcon() {
-        // getEc2InstanceDetails(this.instanceId)
-        //     .then(this.setEc2DetailState)
-        //     .then(console.log.bind(console));
     }
 
     renderTaskListEntry(task) {
@@ -54,28 +36,28 @@ class Agent extends Component {
             </li>
         );
     }
-    
+
     render() {
         const details = this.props.agentDetails;
         const taskListItems = details.tasks.map(this.renderTaskListEntry, this);
+        const instanceType = details.instance.attributes.find(filterForInstanceType);
         return (
             <div className="agent col" id={`agent-${this.instanceId}`}>
                 <Tooltip
-                        tooltipId={`tooltip-${this.instanceId}`}
-                        triggerId={`#agent-${this.instanceId} .agent-icon`}
-                        tippySettings={this.tooltipSettings}>
+                    tooltipId={`tooltip-${this.instanceId}`}
+                    triggerId={`#agent-${this.instanceId} .agent-icon`}
+                    tippySettings={this.tooltipSettings}>
                     <ul className="tooltip-details-list">
                         <li>Status: {details.instance.status}</li>
-                        <li>Instance type: {details.instance.attributes[2].value}</li>
+                        <li>Instance type: {instanceType.value}</li>
                         <li>Registered CPU: {details.instance.registeredResources[0].integerValue} units</li>
                         <li>Remaining CPU: {details.instance.remainingResources[0].integerValue} units</li>
                         <li>Registered Memory: {details.instance.registeredResources[1].integerValue} mb</li>
                         <li>Remaining Memory: {details.instance.remainingResources[1].integerValue} mb</li>
                     </ul>
-                </Tooltip> 
+                </Tooltip>
                 <div className="agent-info">
-                    <i className="agent-icon small material-icons"
-                        onMouseOver={this.handleMouseOverIcon}>perm_identity</i>
+                    <i className="agent-icon small material-icons">perm_identity</i>
                     <strong className="instanceid">{this.instanceId}</strong>
                 </div>
                 <em className="task-count">{details.tasks.length} tasks</em>
